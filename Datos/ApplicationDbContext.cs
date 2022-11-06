e1b0c9bd3b3e1700c66ef6757cc07283ba025c86
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2022-11-06 18:34:57 -0300

    agrego mi dbContext

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Lugar> Lugar{ get; set; }
+    }
+}
